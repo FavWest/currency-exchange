6feefd09a0a4c8c12ed458de6150cda8ac63348e
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-23 15:25:27 -0700

    BUGFIX: produce correct conversion that accounts for dollar amount

--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,8 +1,8 @@
 export default class Exchange {  
-  static getExchange(currency) {
+  static getExchange(currency, amount) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/10?=`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${amount}?=`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -6,19 +6,20 @@ import Exchange from './js/exchange.js';
 //import calculateConversion from './js/calculateConversion.js';
 
 $(document).ready(function() {
-  let conversion;
   $("#convert").click(function(event){
     event.preventDefault();
     let currency=$("#currency-select").val();
-    let promise = Exchange.getExchange(currency);
+    let amount=$("#dollars").val();
+    let promise = Exchange.getExchange(currency, amount);
     promise.then(function(response){
       let body = JSON.parse(response);
       console.log(body);
       console.log(body.conversion_result);
-      conversion=body.conversion_result;
+      let conversion=body.conversion_result;
       $("#display-conversion").text(conversion);
     }, function(error){
       console.error("This is Tori's Error message"+error);
+      $("#error-message").text(error.type);
     });
     $("#display-dollars").text($("#dollars").val());
     $("#display-currency").text($("#currency-select").val());  
