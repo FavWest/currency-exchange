1ee599210af4a1cc7a52bac68e626433ebc33e39
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-23 09:03:28 -0700

    add correct title and author

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Example',
+      title: 'Currency Exchange',
       template: './src/index.html',
       inject: 'body'
     }),
