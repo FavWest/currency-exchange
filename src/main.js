03955d16898df5339712e016a2cc5cfdd2f219ad
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-23 12:41:15 -0700

    WIP: investigating API returning 'blocked by CORS policy' message

--- src/main.js
+++ src/main.js
@@ -2,13 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import Example from './example.js';
+import Exchange from './js/exchange.js';
 
 $(document).ready(function() {
   $("#convert").click(function(event){
     event.preventDefault();
+    let conversion;
+    Exchange.getExchange()
+    .then(function(response){
+      const body = JSON.parse(response);
+      conversion=body.main.conversion_result;
+    }, function(error){
+      console.error("This is Tori's Error message"+error);
+    });
     $("#display-dollars").text($("#dollars").val());
     $("#display-currency").text($("#currency-select").val());
+    $("#display-conversion").text(conversion);
     $("#display-result").show();
   });
 });
