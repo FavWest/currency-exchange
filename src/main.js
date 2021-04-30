5aee2f74b28523f301b94983b16932d44ddc833e
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-29 19:57:41 -0700

    refactor: turn code blocks into functions

--- src/main.js
+++ src/main.js
@@ -4,44 +4,67 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from './js/exchange.js';
 
+function showNeedAmount(){
+  $("#display-need-amount").show();
+  $("#display-result").hide();
+  $("#display-error").hide();
+  $("#display-loading").hide();
+}
+
+function showLoading(){
+  $("#display-need-amount").hide();  
+  $("#display-result").hide();
+  $("#display-error").hide();
+  $("#display-loading").show();
+}
+
+function showConversionResult(response){
+  let conversion=response.conversion_result;
+  $("#display-conversion").text(conversion);
+  $("#display-dollars").text($("#dollars").val());
+  $("#display-currency").text($("#currency-select").val());  
+  $("#display-result").show();
+  $("#display-loading").hide();
+  $("#display-error").hide();
+}
+
+function showAPIError(response){
+  if(response["error-type"]==="invalid-key") {
+    $("#error-message").append(`${response["error-type"]}: make sure you have a valid API key. See project README for instructions for adding an API key.`);
+  } else if(response["error-type"]==="unsupported-code"){
+    $("#error-message").append(`${response["error-type"]}: currency not found; check your currency code.`);
+  } else {
+    $("#error-message").append(response["error-type"]);
+  }
+  $("#display-error").show();
+  $("#display-result").hide();
+  $("#display-loading").hide();
+}
+
+function showFetchError(response) {
+  $("#error-message").append(response.message);
+  $("#display-error").show();
+  $("#display-result").hide();
+  $("#display-loading").hide();
+}
+
 $(document).ready(function() {
   $("#convert").click(function(event){
     event.preventDefault();
     let amount=$("#dollars").val();
     if (amount==="") {
-      $("#display-need-amount").show();
+      showNeedAmount();
     } else {
-      $("#display-need-amount").hide();
       let currency=$("#currency-select").val();
-      $("#display-result").hide();
-      $("#display-error").hide();
-      $("#display-loading").show();
+      showLoading();
       Exchange.getExchange(currency, amount)
         .then(function(response){
           if(response.result==="success"){
-            let conversion=response.conversion_result;
-            $("#display-conversion").text(conversion);
-            $("#display-dollars").text($("#dollars").val());
-            $("#display-currency").text($("#currency-select").val());  
-            $("#display-result").show();
-            $("#display-loading").hide();
-            $("#display-error").hide();
+            showConversionResult(response);
           } else if(response.result==="error") {
-            if(response["error-type"]==="invalid-key") {
-              $("#error-message").append(`${response["error-type"]}: make sure you have a valid API key. See project README for instructions for adding an API key.`);
-            } else if(response["error-type"]==="unsupported-code"){
-              $("#error-message").append(`${response["error-type"]}: currency not found; check your currency code.`);
-            } else {
-              $("#error-message").append(response["error-type"]);
-            }
-            $("#display-error").show();
-            $("#display-result").hide();
-            $("#display-loading").hide();
+            showAPIError(response);
           } else {
-            $("#error-message").append(response.message);
-            $("#display-error").show();
-            $("#display-result").hide();
-            $("#display-loading").hide();
+            showFetchError(response);
           }
         });
     }
